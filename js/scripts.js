1dbf00729de1f52509f6b6c22643d698cd882d78
Matthew Banse-Fay <matthew.bansefay@gmail.com>
2021-11-07 17:22:41 -0800

    Update function to display as new list elements.
    Co-authored-by: Jack <JTSkelton@users.noreply.github.com>
    Co-authored-by: RosarioRu <RosarioRu@users.noreply.github.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,3 @@
-// const colors = ["red", "blue", "blue"];
-// function filterItems(arr,query) {
-//     return arr.filter(function(el) {
-//         return el.toLowerCase().indexOf(query.toLowerCase()) !== -1
-//     })
-// }
-
-// return(filterItems(colors, "blue"));
-// filterItems(colors, colors[1]);
-// for an array called "shapes":
-
-// let shapes = [red,red,red,blue,green];
-// shapes.forEach(element=> console.log(element));
-
-  
 $(document).ready(function() {
   $("#form-text").submit(function(event) {
     event.preventDefault();
@@ -30,16 +15,21 @@ $(document).ready(function() {
         else ++b[b.length - 1];
         previousWord = newWord;
       }
+
+      a.forEach(function(el, index) {
+        a[index] = el + " Times Occured: " + b[index];
+      });
     
-      return [a, b];
+      return a;
     }
-    
-    const result = foo(newArray);
-    console.log('[' + result[0] + ']'+'[' + result[1] + ']')
-    console.log(newArray)
+
+    const results = foo(newArray);
+    results.forEach(function(result) {
+      let word = "<li>" + result + "</li>";
+      $(".results ul").append(word);
+    });
    
   });
 });
 
 
-//words.filter(word => word.length > 6);
